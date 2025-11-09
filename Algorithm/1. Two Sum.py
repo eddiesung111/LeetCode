c9d81ef00e3e8579b544efbc48d7df142a3fbabb
Eddie <eddiesung@gmail.com>
2025-11-09 08:22:35 +0000

    update on 09/11/2025

---
+++ Algorithm/1. Two Sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        n = len(nums)
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[nums[i]] = i
+
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        numMap = {}
+
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+        return []
